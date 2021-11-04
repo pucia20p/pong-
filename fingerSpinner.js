9759e856286f7d453f03e2d65316f70adb878b51
pucia20p <p.pucia.p20@gmail.com>
2021-11-04 15:58:38 +0100

    wooo kręci się
    btw nie wiem co zmieniłem w css ale git

--- fingerSpinner.js
+++ fingerSpinner.js
@@ -11,12 +11,6 @@ for(i=0; i<4; i++){
 }
 const kolko = document.querySelector(".kolko");
 
-
-function rando(){
-    return Math.round(Math.random()*360)
-}
-
-
 let mons = 1000;
 money.innerText = mons;
 
@@ -79,5 +73,63 @@ przycisk[3].addEventListener("click",function(){
 })
 
 
+//zrobić clicka na kółko i losowanie
+
+//losowanie od 0 do 360, zależnie od wylosowanej liczby kręci kółkiem o tyle + 10*360'
+//sprawdza wylosowaną liczbę do tablic z numerkami i daje taki bonus jak się skończy kręcić
+
+//90
+
+let blue = [ 4,5,6,7,8,15,16,17,18,19,20,26,27,28,29,30,31,37,38,39,40,41,42,49,50,51,52,53,60,61,62,63,64,65,71,72,73,74,75,76,82,83,84,85,86,87,94,95,96,97,98,105,106,107,108,109,110,116,117,118,119,120,121,127,128,129,130,131,132,139,140,141,142,143,144,150,151,152,153,154,155,161,162,163,164,165,166,173,174,175,176,177,184,185,186,187,188,195,196,197,198,199,200,206,207,208,209,210,211,217,218,219,220,221,222,228,229,230,231,232,233,240,241,242,243,244,251,252,253,254,255,256,262,263,264,265,266,267,274,275,276,277,278,285,286,287,288,289,296,297,298,299,300,301,307,308,309,310,311,312,318,319,320,321,322,323,330,331,332,333,334,335,341,342,343,344,345,346,352,353,354,355,356,357]
+let red = [ 9,10,11,12,13,14,21,22,23,24,25,32,33,34,35,36,54,55,56,57,58,59,66,67,68,69,70,77,78,79,80,81,99,100,101,102,103,104,111,112,113,114,115,123,124,125,126,145,146,147,148,149,156,157,158,159,160,167,168,169,170,171,172,189,190,191,192,193,194,201,202,203,204,205,212,213,214,215,216,234,235,236,237,238,239,245,246,247,248,249,250,257,258,259,260,261,279,280,281,282,283,284,290,291,292,293,294,295,302,303,304,305,306,324,325,326,327,328,329,336,337,338,339,340,347,348,349,350,351]
+let green = [ 1,2,3,43,44,45,46,47,48,88,89,90,91,92,93,133,134,135,136,137,138,223,224,225,226,227,268,269,270,271,272,273,313,314,315,316,317,358,359,360]
+let gold = [ 178,179,180,181,182,183]
+let wlacz = true
+kolko.addEventListener("click",function (){
+    if(wlacz){
+        wlacz = false
+        let losowanie = Math.ceil(Math.random()*360)
+        let wyn = losowanie + (3*360)
+        let color;
+        kolko.style.transitionDuration = "5s" 
+        kolko.style.transform = `rotate(${wyn}deg)`;
+        setTimeout(function(){
+            
+            for(i=0; i<blue.length; i++){
+                if(blue[i] == losowanie)
+                    color = 1
+            }
+            if(color == null){
+                for(i=0; i<red.length; i++){
+                    if(red[i] == losowanie)
+                        color = 2
+                }   
+            }
+            if(color == null){
+                for(i=0; i<green.length; i++){
+                    if(green[i] == losowanie)
+                        color = 3
+                }   
+            }
+            if(color == null){
+                for(i=0; i<gold.length; i++){
+                    if(gold[i] == losowanie)
+                        color = 4
+                }   
+            }
+            console.log(color)
+            
+        },6000)
+        setTimeout(function(){
+            kolko.style.transitionDuration = "1s"
+            kolko.style.transform = `rotate(0deg)`
+            
+        }, 8000)
+        console.log(losowanie)
+        setTimeout(function (){
+            wlacz = true
+        }, 9500)
+    }
+})
 
 
